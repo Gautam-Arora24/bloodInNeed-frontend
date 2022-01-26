138a3347e957fee971cb300ba7242f34479161ec
Gautam Arora <gautamarora6248@gmail.com>
2022-01-26 15:03:50 +0530

    Changed Hosting service from Heroku to railway.app

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ export default function App() {
   }
 
   const httpLink = createHttpLink({
-    uri: "https://blood-in-need.herokuapp.com/",
+    uri: "https://bloodinneed-backend-production.up.railway.app/",
   });
   const authLink = setContext((_, { headers }) => {
     // get the authentication token from local storage if it exists
